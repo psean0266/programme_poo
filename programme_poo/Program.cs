de9fb8b969b1f1db0cfac48e94f4de7cacf5b7ca
Abdoulaye Pangnon KOULIBALY <abdoulaye-pangnon.koulibaly@talan.com>
2023-11-07 22:02:15 +0100

    second commit

--- programme_poo/Program.cs
+++ programme_poo/Program.cs
@@ -3,28 +3,54 @@ using System.Collections.Generic;
 
 namespace programme_poo
 {
-     class Program
+    class Personne
     {
+        string nom;
+        int age;
+        string emploi;
 
-        static void AfficherInfosPersonne(string nom, int age, string emploi)
+        public Personne(string nom, int age, string emploi)
+        {
+            this.nom = nom;
+            this.age = age;
+            this.emploi = emploi;
+        }
+
+        public void Afficher()
         {
             Console.WriteLine("NOM : " + nom);
             Console.WriteLine(" AGE : " + age + "ans");
             Console.WriteLine("EMPLOI : " + emploi);
         }
+    }
+     class Program
+    {
+
+        //static void AfficherInfosPersonne(string nom, int age, string emploi)
+        //{
+        //    Console.WriteLine("NOM : " + nom);
+        //    Console.WriteLine(" AGE : " + age + "ans");
+        //    Console.WriteLine("EMPLOI : " + emploi);
+        //}
         static void Main(string[] args)
         {
             // nom, age, emploi
 
-            var noms = new List<string> { "Paul", "Jacques", "David" };
-            var ages = new List<int> { 30, 35, 20 };
-            var emploi = new List<string> { "Développeur", "Professeur", "Etudiant" };
+            //var noms = new List<string> { "Paul", "Jacques", "David" };
+            //var ages = new List<int> { 30, 35, 20 };
+            //var emploi = new List<string> { "Développeur", "Professeur", "Etudiant" };
 
-            for (int i = 0; i < noms.Count; i++)
-            {
-                AfficherInfosPersonne(noms[i], ages[i], emploi[i]);
+            //for (int i = 0; i < noms.Count; i++)
+            //{
+            //    AfficherInfosPersonne(noms[i], ages[i], emploi[i]);
 
-            }
+            //}
+
+            Personne personne1 = new Personne("Paul", 20, "Etudiant");
+            personne1.Afficher(); 
+
+            Personne personne2 = new Personne("Jacques", 38, "Professeur");
+            personne2.Afficher();   
         }
     }
 }
