2f72ee6f9a5716f71bf011b4fe61332085141a74
Abdoulaye Pangnon KOULIBALY <abdoulaye-pangnon.koulibaly@talan.com>
2023-11-08 05:13:35 +0100

    liste personne

--- programme_poo/Program.cs
+++ programme_poo/Program.cs
@@ -19,7 +19,7 @@ namespace programme_poo
         public void Afficher()
         {
             Console.WriteLine("NOM : " + nom);
-            Console.WriteLine(" AGE : " + age + "ans");
+            Console.WriteLine(" AGE : " + age + " ans");
             Console.WriteLine("EMPLOI : " + emploi);
         }
     }
@@ -47,10 +47,28 @@ namespace programme_poo
             //}
 
             Personne personne1 = new Personne("Paul", 20, "Etudiant");
-            personne1.Afficher(); 
+          //  personne1.Afficher(); 
 
             Personne personne2 = new Personne("Jacques", 38, "Professeur");
-            personne2.Afficher();   
+          //  personne2.Afficher();   
+
+           var personnes = new List<Personne> {
+           new Personne("Jacques", 38, "Professeur"),
+           new Personne("Noe", 38, "ETUDIANT"),
+           new Personne("Farid", 38, "Comptable"),
+           new Personne("Kaaris", 38, "Artiste"),
+           new Personne("Megane", 38, "Tiktokeuse"),
+        };
+
+            //for (int i = 0; i < personnes.Count; i++)
+            //{
+            //    personnes[i].Afficher();
+            //  //  Console.WriteLine(" Personnes : " + personnes[i]);
+            //}
+
+            foreach (Personne personne in personnes) {
+            personne.Afficher();
+            }
         }
     }
 }
