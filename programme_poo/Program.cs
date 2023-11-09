bc49d423850727d515d4de7859e739281f070d99
Abdoulaye Pangnon KOULIBALY <abdoulaye-pangnon.koulibaly@talan.com>
2023-11-09 06:06:02 +0100

    constructeur avec propriété facultative

--- programme_poo/Program.cs
+++ programme_poo/Program.cs
@@ -6,13 +6,45 @@ namespace programme_poo
 {
     class Personne
     {
-        public static int nombrePersonnes = 0; 
+        private static int nombrePersonnes = 0; 
         public string nom;
         int age;
         string emploi;
         int numeroPersonne;
 
-        public Personne(string nom, int age, string emploi)
+        #region Les méthode décomposé du constructeur
+        //public Personne(string nom, int age)
+        //{
+        //    this.nom = nom;
+        //    this.age = age;
+        //    this.emploi = "(Non spécifié)";
+
+        //    nombrePersonnes++;
+
+        //    this.numeroPersonne = nombrePersonnes;
+        //}
+
+
+        //public Personne(string nom, int age, string emploi = "(Non spécifié)")
+        //{
+        //    this.nom = nom;
+        //    this.age = age;
+        //    this.emploi = emploi;
+
+        //    nombrePersonnes++;
+
+        //    this.numeroPersonne = nombrePersonnes;
+        //}
+
+        #endregion
+
+        #region constructeur composé pour faire rendre facultatif emploi 
+
+        //public Personne(string nom, int age): this(nom, age, "(Non spécifié)")
+        //{
+      
+        //}
+        public Personne(string nom, int age, string emploi = null )
         {
             this.nom = nom;
             this.age = age;
@@ -23,17 +55,33 @@ namespace programme_poo
             this.numeroPersonne = nombrePersonnes;
         }
 
+        #endregion
+        public static void AfficherNombreDePersonnes()
+        {
+            Console.WriteLine(" Nombre total de numéro " +nombrePersonnes);
+        }
+
         public void Afficher()
         {
             Console.WriteLine("PERSONNE N° : " + numeroPersonne);
             Console.WriteLine("NOM : " + nom);
             Console.WriteLine(" AGE : " + age + " ans");
-            Console.WriteLine("EMPLOI : " + emploi);
+            if(emploi == null)
+            {
+                Console.WriteLine("Aucun emploi spécifié");
+            }
+
+            else
+            {
+                Console.WriteLine("EMPLOI : " + emploi);
+            }
+            
+            
+            
         }
     }
      class Program
     {
-
         //static void AfficherInfosPersonne(string nom, int age, string emploi)
         //{
         //    Console.WriteLine("NOM : " + nom);
@@ -54,35 +102,35 @@ namespace programme_poo
 
             //}
 
-           // Personne personne1 = new Personne("Paul", 20, "Etudiant");
-          //  personne1.Afficher(); 
+            // Personne personne1 = new Personne("Paul", 20, "Etudiant");
+            //  personne1.Afficher(); 
 
-           // Personne personne2 = new Personne("Jacques", 38, "Professeur");
-          //  personne2.Afficher();   
+            // Personne personne2 = new Personne("Jacques", 38, "Professeur");
+            //  personne2.Afficher();   
+            /*
+                       var personnes = new List<Personne> {
+                       new Personne("Jacques", 38, "Professeur"),
+                       new Personne("Noe", 38, "ETUDIANT"),
+                       new Personne("Farid", 38, "Comptable"),
+                       new Personne("Kaaris", 38, "Artiste"),
+                       new Personne("Megane", 38, "Tiktokeuse"),
+                       new Personne("Aristide", 38, "Footballeur"),
+                    };
 
-           var personnes = new List<Personne> {
-           new Personne("Jacques", 38, "Professeur"),
-           new Personne("Noe", 38, "ETUDIANT"),
-           new Personne("Farid", 38, "Comptable"),
-           new Personne("Kaaris", 38, "Artiste"),
-           new Personne("Megane", 38, "Tiktokeuse"),
-           new Personne("Aristide", 38, "Footballeur"),
-        };
+                        //for (int i = 0; i < personnes.Count; i++)
+                        //{
+                        //    personnes[i].Afficher();
+                        //  //  Console.WriteLine(" Personnes : " + personnes[i]);
+                        //}
+                        //personnes = personnes.OrderBy(p => p.nom).ToList(); 
+                        foreach (Personne personne in personnes) {
+                        personne.Afficher();
+                            //personne.nombrePersonnes;
+                        }
+                        Personne.AfficherNombreDePersonnes(); */
 
-            //for (int i = 0; i < personnes.Count; i++)
-            //{
-            //    personnes[i].Afficher();
-            //  //  Console.WriteLine(" Personnes : " + personnes[i]);
-            //}
-            //personnes = personnes.OrderBy(p => p.nom).ToList(); 
-            foreach (Personne personne in personnes) {
-            personne.Afficher();
-                //personne.nombrePersonnes;
-            }
-
-            
-            Console.WriteLine(" Nombre total de numéro "+ Personne.nombrePersonnes);
-
+            var personne1 = new Personne("Paul", 30, "ingénieur");
+            personne1.Afficher();
         }
     }
 }
